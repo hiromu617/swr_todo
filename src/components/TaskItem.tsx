c1d28cd41c71a4ea87237e46d496101e1fa3c0da
hiromu617 <hiromhiro617@gmail.com>
2022-12-11 16:45:07 +0900

    impl delete task

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -2,13 +2,24 @@ import { Card, CardBody, Text, Checkbox, IconButton } from "@chakra-ui/react";
 import { FC } from "react";
 import { CloseIcon } from "@chakra-ui/icons";
 import { supabase } from "../client";
+import { useSWRConfig } from "swr";
 import { Task } from "../types";
 
 type Props = {
   task: Task;
 };
 
+const deleteTask = async (id: number) => {
+  await supabase.from("tasks").delete().eq("id", id);
+};
+
 export const TaskItem: FC<Props> = ({ task }) => {
+  const { mutate } = useSWRConfig();
+  const handleDeleteTask = async () => {
+    await deleteTask(task.id);
+    mutate("/tasks");
+  };
+
   return (
     <Card key={task.id}>
       <CardBody
@@ -24,6 +35,7 @@ export const TaskItem: FC<Props> = ({ task }) => {
           size="sm"
           colorScheme="red"
           icon={<CloseIcon />}
+          onClick={handleDeleteTask}
         />
       </CardBody>
     </Card>
